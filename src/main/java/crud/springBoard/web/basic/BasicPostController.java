7527475fc5afd822cade6d447bda34bec9ac922d
andrew <statco19@gmail.com>
2022-01-30 20:34:22 +0900

    implemented a list of articles with th

--- src/main/java/crud/springBoard/web/basic/BasicPostController.java
+++ src/main/java/crud/springBoard/web/basic/BasicPostController.java
@@ -1,9 +1,34 @@
 package crud.springBoard.web.basic;
 
+import crud.springBoard.domain.post.Post;
+import crud.springBoard.domain.post.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.PostConstruct;
+import java.util.List;
+
 @Controller
-@RequestMapping("")
+@RequestMapping("basic/posts")
+@RequiredArgsConstructor
 public class BasicPostController {
+
+    private final PostRepository postRepository;
+
+    @GetMapping
+    public String posts(Model model) {
+        List<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+
+        return "basic/posts";
+    }
+
+    @PostConstruct
+    public void init() {
+        postRepository.save(new Post("titleA", "my first post."));
+        postRepository.save(new Post("titleB", "my second post."));
+    }
 }
