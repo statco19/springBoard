94a63d160738f0587e79cef02a22725f345534e0
andrew <statco19@gmail.com>
2022-01-30 20:19:03 +0900

    created web html files

---
+++ src/main/java/crud/springBoard/web/basic/BasicPostController.java
@@ -0,0 +1,9 @@
+package crud.springBoard.web.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("")
+public class BasicPostController {
+}
